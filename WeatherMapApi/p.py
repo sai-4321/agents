372caf47fa784d7d9e47b529f6030627e3c37335
SAI SREEE BEERAVELLI <164086480+sai-4321@users.noreply.github.com>
2026-03-10 13:19:14 -0500

    Delete WeatherMapApi/p.py

--- WeatherMapApi/p.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-filee=st.file_uploader("upload a file",type=["png","pdf","jpeg","mp3"])
-
-if filee:
-    st.write(filee.type)
-    if filee.type.startswith("image"):
-     st.image(filee,width=300)
